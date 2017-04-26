36634d2dddcd645000690345322f207d75802b48
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-04-26 16:40:38 +0600

    feat(Header): draw mobile version of header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,47 +1,19 @@
 /* @flow */
 
-import {FormattedMessage} from 'react-intl';
-import {Link} from 'phenomic';
-import React, {PropTypes} from 'react';
-import Svg from 'react-svg-inline';
+import React from 'react';
 
-import gitHubSvg from '../icons/iconmonstr-github-1.svg';
 import styles from './index.css';
-import twitterSvg from '../icons/iconmonstr-twitter-1.svg';
+import logo from './logo.png';
 
-type ContextType = {|
-  metadata: Phenomic$Metadata,
-|};
-
-const Header = (props: Object, {metadata: {pkg}}: ContextType) => (
+const Header = () => (
   <header className={styles.header}>
-    <nav className={styles.nav}>
-      <div className={styles.navPart1}>
-        <Link className={styles.link} to={'/'}>
-          <FormattedMessage id="header.title" />
-        </Link>
-      </div>
-      <div className={styles.navPart2}>
-        {pkg.twitter &&
-          <a
-            href={`https://twitter.com/${pkg.twitter}`}
-            className={styles.link}
-          >
-            <Svg svg={twitterSvg} cleanup />
-            {'Twitter'}
-          </a>}
-        {pkg.repository &&
-          <a href={pkg.repository} className={styles.link}>
-            <Svg svg={gitHubSvg} cleanup />
-            {'GitHub'}
-          </a>}
-      </div>
-    </nav>
+    <div className={styles.logoContainer}>
+      <img className={styles.logo} src={logo} alt="Logo" />
+    </div>
+    <div className={styles.burgerContainer}>
+      <button className={styles.burger}>☰</button>
+    </div>
   </header>
 );
-
-Header.contextTypes = {
-  metadata: PropTypes.object.isRequired,
-};
 
 export default Header;
