9c8ff1f52f00a56d48c5629462c363237eb4e218
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-04-28 20:53:58 +0600

    feat(Header): add responsiveness to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,16 +7,41 @@ import styles from './index.css';
 import logo from './logo.png';
 
 const Header = () => (
-  <header className={styles.header}>
-    <Link className={styles.logoLink} to="/">
-      <img className={styles.logo} src={logo} alt="Logo" />
-    </Link>
-    <a className={styles.burger}>
-      <span className={styles.bar} />
-      <span className={styles.barMid} />
-      <span className={styles.bar} />
-    </a>
-  </header>
+  <div className={styles.headerWrapper}>
+    <header className={styles.header}>
+      <Link className={styles.logoLink} to="/">
+        <img className={styles.logo} src={logo} alt="Logo" />
+      </Link>
+      <nav className={styles.nav}>
+        <Link className={styles.about} to="/">
+          О нас
+        </Link>
+        <Link className={styles.development} to="/">
+          Услуги разработки
+        </Link>
+        <Link className={styles.startups} to="/">
+          Наши стартапы
+        </Link>
+        <Link className={styles.courses} to="/">
+          Курсы
+        </Link>
+        <Link className={styles.knowledge} to="/">
+          Knowledge Base
+        </Link>
+        <Link className={styles.videos} to="/">
+          Наши видео
+        </Link>
+        <Link className={styles.blog} to="/">
+          Блог
+        </Link>
+      </nav>
+      <a className={styles.burger}>
+        <span className={styles.bar} />
+        <span className={styles.barMid} />
+        <span className={styles.bar} />
+      </a>
+    </header>
+  </div>
 );
 
 export default Header;
