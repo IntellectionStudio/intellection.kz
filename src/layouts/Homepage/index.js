39bf9a0b44cbb0a1589869bfe1a07ae639e8913a
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-02 12:31:52 +0600

    feat(Homepage): rename heroModule to cards

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -15,7 +15,7 @@ type PropsType = {
 const Homepage = (props: PropsType) => (
   <Page {...props}>
     <HeroVideo />
-    <div className={styles.heroModule}>
+    <div className={styles.cards}>
       <Card />
       <Card />
       <Card />
