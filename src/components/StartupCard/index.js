206d72a785f3d73c5ae9256fd678ece0a643c25a
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-10 14:00:52 +0600

    feat(StartupCard): add image

--- src/components/StartupCard/index.js
+++ src/components/StartupCard/index.js
@@ -6,7 +6,13 @@ import styles from './index.css';
 
 const StartupCard = () => (
   <div className={styles.container}>
-    <div className={styles.picture} />
+    <div className={styles.imageContainer}>
+      <img
+        className={styles.image}
+        src="http://is2.mzstatic.com/image/thumb/Purple111/v4/d4/e3/cf/d4e3cf6b-bdb0-26c0-8553-4683955968a2/source/175x175bb.jpg"
+        alt="Qazaq App"
+      />
+    </div>
     <div className={styles.info}>
       <h2 className={styles.title}>Qazaq App</h2>
       <p className={styles.description}>
