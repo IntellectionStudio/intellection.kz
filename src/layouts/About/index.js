af5455d251bd8acb69af9726c604e6b99e0113c5
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-16 19:59:39 +0600

    feat: resize image container with react-measure

--- src/layouts/About/index.js
+++ src/layouts/About/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import React from 'react';
+import React, {Component} from 'react';
+import Measure from 'react-measure';
 import {mapObjIndexed, values} from 'ramda';
 
 import Page from 'layouts/Page';
@@ -8,10 +9,6 @@ import TopManagerCard from 'components/TopManagerCard';
 import TeamMemberCard from 'components/TeamMemberCard';
 
 import styles from './index.css';
-
-type PropsType = {
-  head: Object,
-};
 
 const renderTopManagerCard = (topManager, key) => (
   <TopManagerCard key={key} {...topManager} />
@@ -21,53 +18,79 @@ const renderTeamMemberCard = (teamMember, key) => (
 );
 const mapVal = (fn, obj) => values(mapObjIndexed(fn, obj));
 
-const Homepage = (props: PropsType) => (
-  <Page {...props}>
-    <div className={styles.hero}>
-      <div className={styles.heroContent}>
-        <h1 className={styles.heroTitle}>
-          {props.head.hero.title}
-        </h1>
-        <p className={styles.heroText}>
-          {props.head.hero.text}
-        </p>
-      </div>
-    </div>
-    <div className={styles.video}>
-      <div className={styles.videoContent}>
-        <h1 className={styles.videoTitle}>
-          {props.head.video.title}
-        </h1>
-        <p className={styles.videoText}>
-          {props.head.hero.text}
-        </p>
-        <a className={styles.videoButton}>
-          {props.head.video.buttonText}
-          <img
-            className={styles.playIcon}
-            src="/assets/play-blue.svg"
-            alt="Play"
-          />
-        </a>
-      </div>
-    </div>
-    <div className={styles.team}>
-      <div className={styles.teamContent}>
-        <h1 className={styles.teamTitle}>
-          {props.head.team.title}
-        </h1>
-        <p className={styles.teamText}>
-          {props.head.team.text}
-        </p>
-      </div>
-    </div>
-    <div className={styles.management}>
-      {mapVal(renderTopManagerCard, props.head.team.topManagers)}
-    </div>
-    <div className={styles.teamMembers}>
-      {mapVal(renderTeamMemberCard, props.head.team.teamMembers)}
-    </div>
-  </Page>
-);
+class Homepage extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      videoFrameLength: 0,
+    };
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <Page {...props}>
+        <div className={styles.hero}>
+          <div className={styles.heroContent}>
+            <h1 className={styles.heroTitle}>
+              {props.head.hero.title}
+            </h1>
+            <p className={styles.heroText}>
+              {props.head.hero.text}
+            </p>
+          </div>
+        </div>
+        <Measure
+          onMeasure={dimensions =>
+            console.log('======', dimensions) ||
+            this.setState({videoFrameLength: dimensions.height})}
+        >
+          <div className={styles.video}>
+            <div className={styles.videoContent}>
+              <h1 className={styles.videoTitle}>
+                {props.head.video.title}
+              </h1>
+              <p className={styles.videoText}>
+                {props.head.hero.text}
+              </p>
+              <a className={styles.videoButton}>
+                {props.head.video.buttonText}
+                <img
+                  className={styles.playIcon}
+                  src="/assets/play-blue.svg"
+                  alt="Play"
+                />
+              </a>
+            </div>
+            <div
+              className={styles.videoFrame}
+              style={{
+                height: `${this.state.videoFrameLength}px`,
+                width: `${this.state.videoFrameLength}px`,
+              }}
+            />
+          </div>
+        </Measure>
+        <div className={styles.team}>
+          <div className={styles.teamContent}>
+            <h1 className={styles.teamTitle}>
+              {props.head.team.title}
+            </h1>
+            <p className={styles.teamText}>
+              {props.head.team.text}
+            </p>
+          </div>
+        </div>
+        <div className={styles.management}>
+          {mapVal(renderTopManagerCard, props.head.team.topManagers)}
+        </div>
+        <div className={styles.teamMembers}>
+          {mapVal(renderTeamMemberCard, props.head.team.teamMembers)}
+        </div>
+      </Page>
+    );
+  }
+}
 
 export default Homepage;
