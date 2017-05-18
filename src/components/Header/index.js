5e124a653c947bc1221f7874758f487a7b542c57
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-18 17:41:11 +0600

    feat(Header): use button instead of a tag

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'phenomic';
 import cx from 'classnames';
 
@@ -10,57 +10,109 @@ type PropsType = {
   white: boolean,
 };
 
-const Header = ({white}: PropsType) => (
-  <div
-    className={cx(styles.headerWrapper, {
-      [styles.white]: white,
-    })}
-  >
-    <header className={styles.header}>
-      <Link className={styles.logoLink} to="/">
-        {white
-          ? <img className={styles.logo} src="/assets/logo2.png" alt="Logo" />
-          : <img className={styles.logo} src="/assets/logo.png" alt="Logo" />}
-      </Link>
-      <nav className={styles.nav}>
-        <Link className={styles.about} to="/about">
-          О нас
-        </Link>
-        <Link className={styles.startups} to="/">
-          Наши стартапы
-        </Link>
-        <Link className={styles.courses} to="/">
-          Курсы
-        </Link>
-        <Link className={styles.knowledge} to="/">
-          Knowledge Base
-        </Link>
-        <Link className={styles.videos} to="/">
-          Наши видео
-        </Link>
-        <Link className={styles.blog} to="/">
-          Блог
-        </Link>
-      </nav>
-      <a className={styles.burger}>
-        <span
-          className={cx(styles.bar, {
-            [styles.barBlue]: white,
-          })}
-        />
-        <span
-          className={cx(styles.bar, {
-            [styles.barMidBlue]: white,
-          })}
-        />
-        <span
-          className={cx(styles.bar, {
-            [styles.barBlue]: white,
-          })}
-        />
-      </a>
-    </header>
-  </div>
-);
+class Header extends Component {
+  props: PropsType;
+  state = {
+    open: false,
+  };
+
+  render() {
+    const {white} = this.props;
+    return (
+      <div
+        className={cx(styles.headerWrapper, {
+          [styles.white]: white,
+        })}
+      >
+        <header className={styles.header}>
+          <Link className={styles.logoLink} to="/">
+            {white
+              ? <img
+                  className={styles.logo}
+                  src="/assets/logo2.png"
+                  alt="Logo"
+                />
+              : <img
+                  className={styles.logo}
+                  src="/assets/logo.png"
+                  alt="Logo"
+                />}
+          </Link>
+          <nav
+            className={cx(styles.nav, {
+              [styles.open]: this.state.open,
+            })}
+          >
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/about"
+            >
+              О нас
+            </Link>
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/"
+            >
+              Наши стартапы
+            </Link>
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/"
+            >
+              Курсы
+            </Link>
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/"
+            >
+              Knowledge Base
+            </Link>
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/"
+            >
+              Наши видео
+            </Link>
+            <Link
+              className={cx(styles.navItem, {
+                [styles.navItemOpen]: this.state.open,
+              })}
+              to="/"
+            >
+              Блог
+            </Link>
+          </nav>
+          <button className={styles.burger} onClick={this.toggleNav}>
+            <span
+              className={cx(styles.bar, {
+                [styles.barBlue]: white,
+              })}
+            />
+            <span
+              className={cx(styles.bar, {
+                [styles.barMidBlue]: white,
+              })}
+            />
+            <span
+              className={cx(styles.bar, {
+                [styles.barBlue]: white,
+              })}
+            />
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default Header;
