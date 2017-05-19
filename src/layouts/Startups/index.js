6d1634de0b9929aecd34ede6386d45cc9382b0ce
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-19 11:15:16 +0600

    feat(Statups): add slider

--- src/layouts/Startups/index.js
+++ src/layouts/Startups/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import React from 'react';
+import Slider from 'react-slick';
 
 import Page from 'layouts/Page';
 
@@ -13,7 +14,14 @@ type PropsType = {
 const Homepage = (props: PropsType) => (
   <Page {...props}>
     <div className={styles.container}>
-      Our Startups
+      <Slider infinite={false} slidesToShow={5} slidesToScroll={1}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
     </div>
   </Page>
 );
