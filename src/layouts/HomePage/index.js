a9064b13129988b5f465c1242dab0c6a46f43bdc
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-05-29 17:42:08 +0600

    feat(HomePage): remove startup cards

--- src/layouts/HomePage/index.js
+++ src/layouts/HomePage/index.js
@@ -1,7 +1,6 @@
-import {Link} from 'phenomic';
 import React from 'react';
 
-import {Card, HeroVideo, StartupCard} from 'components';
+import {Card, HeroVideo} from 'components';
 import mapValues from 'utils/mapValues';
 import Page from 'layouts/Page';
 
@@ -10,12 +9,6 @@ import styles from './index.css';
 const renderCard = card => (
   <Card key={`${card.title}-${card.text}`} card={card} />
 );
-const renderStartupCard = startupCard => (
-  <StartupCard
-    key={`${startupCard.title}-${startupCard.text}`}
-    startupCard={startupCard}
-  />
-);
 
 const HomePage = props => (
   <Page {...Page.pickPageProps(props)}>
@@ -23,42 +16,6 @@ const HomePage = props => (
       <HeroVideo {...props.head.hero.video} />
       <div className={styles.cards}>
         {mapValues(renderCard)(props.head.hero.cards)}
-      </div>
-    </div>
-    <div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          {props.head.startupsSection.title}
-        </h1>
-        <p className={styles.text}>
-          {props.head.startupsSection.text}
-        </p>
-      </div>
-      <div className={styles.startups}>
-        {mapValues(renderStartupCard, props.head.startupsSection.startups)}
-      </div>
-      <div className={styles.buttonContainer}>
-        <Link className={styles.button} to="startups">
-          {props.head.startupsSection.buttonText}
-        </Link>
-      </div>
-    </div>
-    <div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          {props.head.startupsSection.title}
-        </h1>
-        <p className={styles.text}>
-          {props.head.startupsSection.text}
-        </p>
-      </div>
-      <div className={styles.startups}>
-        {mapValues(renderStartupCard)(props.head.startupsSection.startups)}
-      </div>
-      <div className={styles.buttonContainer}>
-        <Link className={styles.button} to="startups">
-          {props.head.startupsSection.buttonText}
-        </Link>
       </div>
     </div>
   </Page>
