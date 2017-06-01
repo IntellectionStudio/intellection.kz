15a374154bab686755af74db86a123e79eca1430
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-01 13:59:57 +0600

    feat: add some wrappers to design ls

--- src/layouts/CoursesPage/index.js
+++ src/layouts/CoursesPage/index.js
@@ -27,72 +27,76 @@ const CoursesPage = props => (
           </div>
         </div>
       </div>
-      <div className={styles.about}>
-        <h2 className={styles.aboutTitle}>
-          Что вам даст этот курс?
-        </h2>
-        <p className={styles.aboutText}>
-          I have hinted that I would often jerk poor Queequeg from between the whale and the ship—where he would occasionally fall, from the incessant.
-        </p>
-        <p className={styles.aboutText}>
-          I have hinted that I would often jerk poor Queequeg from between the whale and the ship—where he would occasionally fall, from the incessant.
-        </p>
-        <ul className={styles.benefits}>
-          <li className={styles.benefit}>Вы научитесь писать код</li>
-          <li className={styles.benefit}>Вы научитесь писать код</li>
-          <li className={styles.benefit}>Вы научитесь писать код</li>
-          <li className={styles.benefit}>Вы научитесь писать код</li>
-        </ul>
-      </div>
-      <div className={styles.statsContainer}>
-        <img
-          className={styles.statsIcon}
-          src="/assets/stats-icon.png"
-          alt="Stats Icon"
-        />
-        <img
-          className={styles.statsPlaceholder}
-          src="/assets/stats-placeholder.png"
-          alt="Paysa Stats"
-        />
-      </div>
-      <div className={styles.individual}>
-        <h2 className={styles.individualTitle}>
-          Индивидуальный Курс
-        </h2>
-        <div className={styles.attributes}>
-          <div className={styles.attribute}>
-            <div className={styles.attributeHeader}>∞</div>
-            <p className={styles.attributeText}>Довольных студентов</p>
-          </div>
-          <div className={styles.attribute}>
-            <div className={styles.attributeHeader}>17</div>
-            <p className={styles.attributeText}>Сломанных телефонов</p>
-          </div>
-          <div className={styles.attribute}>
-            <div className={styles.attributeHeader}>
-              <img
-                className={styles.attributeHeaderImage}
-                src="/assets/apple-icon.png"
-                alt="Apple Icon"
-              />
-            </div>
-            <p className={styles.attributeText}>Сломанных телефонов</p>
-          </div>
-          <div className={styles.attribute}>
-            <div className={styles.attributeHeader}>0</div>
-            <p className={styles.attributeText}>Негативных отзывов</p>
-          </div>
+      <div className={styles.main}>
+        <div className={styles.about}>
+          <h2 className={styles.aboutTitle}>
+            Что вам даст этот курс?
+          </h2>
+          <p className={styles.aboutText}>
+            I have hinted that I would often jerk poor Queequeg from between the whale and the ship—where he would occasionally fall, from the incessant.
+          </p>
+          <p className={styles.aboutText}>
+            I have hinted that I would often jerk poor Queequeg from between the whale and the ship—where he would occasionally fall, from the incessant.
+          </p>
+          <ul className={styles.benefits}>
+            <li className={styles.benefit}>Вы научитесь писать код</li>
+            <li className={styles.benefit}>Вы научитесь писать код</li>
+            <li className={styles.benefit}>Вы научитесь писать код</li>
+            <li className={styles.benefit}>Вы научитесь писать код</li>
+          </ul>
         </div>
-        <div className={styles.individualSummary}>
-          <div className={styles.price}>
-            <h3 className={styles.priceTitle}>60 тыс.</h3>
-            <p className={styles.priceText}>тенге за курс</p>
+        <div className={styles.mainRight}>
+          <div className={styles.statsContainer}>
+            <img
+              className={styles.statsIcon}
+              src="/assets/stats-icon.png"
+              alt="Stats Icon"
+            />
+            <img
+              className={styles.statsPlaceholder}
+              src="/assets/stats-placeholder.png"
+              alt="Paysa Stats"
+            />
           </div>
-          <div className={styles.ctaContainer}>
-            <button className={styles.cta}>
-              Оставить заявку
-            </button>
+          <div className={styles.individual}>
+            <h2 className={styles.individualTitle}>
+              Индивидуальный Курс
+            </h2>
+            <div className={styles.attributes}>
+              <div className={styles.attribute}>
+                <div className={styles.attributeHeader}>∞</div>
+                <p className={styles.attributeText}>Довольных студентов</p>
+              </div>
+              <div className={styles.attribute}>
+                <div className={styles.attributeHeader}>17</div>
+                <p className={styles.attributeText}>Сломанных телефонов</p>
+              </div>
+              <div className={styles.attribute}>
+                <div className={styles.attributeHeader}>
+                  <img
+                    className={styles.attributeHeaderImage}
+                    src="/assets/apple-icon.png"
+                    alt="Apple Icon"
+                  />
+                </div>
+                <p className={styles.attributeText}>Сломанных телефонов</p>
+              </div>
+              <div className={styles.attribute}>
+                <div className={styles.attributeHeader}>0</div>
+                <p className={styles.attributeText}>Негативных отзывов</p>
+              </div>
+            </div>
+            <div className={styles.individualSummary}>
+              <div className={styles.price}>
+                <h3 className={styles.priceTitle}>60 тыс.</h3>
+                <p className={styles.priceText}>тенге за курс</p>
+              </div>
+              <div className={styles.ctaContainer}>
+                <button className={styles.cta}>
+                  Оставить заявку
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
