ed0159819c00098c35a78e608097e0a5ce181244
ak8912 <kasaselya91@gmail.com>
2017-06-10 18:46:46 +0600

    feat: add faceimage functionality back

--- src/components/TeamMemberCard/index.js
+++ src/components/TeamMemberCard/index.js
@@ -1,16 +1,19 @@
 import {pure} from 'recompact';
 import React from 'react';
 
-import {Image} from 'components';
-
+import FaceImage from './FaceImage';
 import styles from './index.css';
 
 const TeamMemberCard = ({
-  teamMember: {image, firstName, lastName, position, text},
+  teamMember: {faceImageCollection, image, firstName, lastName, position, text},
 }) => (
   <div className={styles.container}>
     <div className={styles.profile}>
-      <Image className={styles.image} name={image} alt="intellection faces" />
+      <FaceImage
+        className={styles.image}
+        faceImageCollection={faceImageCollection}
+        defaultImage={image}
+      />
       <h2 className={styles.name}>{firstName} {lastName}</h2>
       <h3 className={styles.position}>{position}</h3>
     </div>
