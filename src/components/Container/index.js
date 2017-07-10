7ae15f7d13493838a372080ed488382b4f409670
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-07-10 20:01:04 +0600

    fix: fix eslint errors

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 import styles from './index.css';
 
-const Container = ({children}) =>
+const Container = ({children}) => (
   <div className={styles.container}>
     {children}
-  </div>;
+  </div>
+);
 
 export default pure(Container);
