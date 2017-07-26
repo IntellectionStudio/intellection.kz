e6268611b032a2d8a16b712e2482e31367f3b476
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-07-26 18:47:20 +0600

    fix: fix google analytics

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-
-import { IntlProvider } from 'react-intl';
+import {IntlProvider} from 'react-intl';
 import React from 'react';
 
-import { Container, Content, DefaultHeadMeta } from 'components';
+import {Container, Content, DefaultHeadMeta} from 'components';
 import getIntlForLocale from 'utils/getIntlForLocale';
 
 import 'normalize.css/normalize.css';
@@ -11,15 +9,11 @@ import 'react-modal-video/scss/modal-video.scss';
 import './index.global.css';
 import './highlight.global.css';
 
+/* eslint-disable */
 // GOOGLE Analytics, part 1/2
 const GOOGLE_ANALYTICS_UA = 'UA-73008937-1';
 if (typeof window !== 'undefined') {
-  window.ga =
-    window.ga ||
-    function () {
-      (ga.q = ga.q || []).push(arguments);
-    };
-  ga.l = Number(new Date()); // eslint-disable-line no-explicit-coercion
+  window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) }; ga.l = +new Date;
   // ga comes from google-analytics script injected below
   ga('create', GOOGLE_ANALYTICS_UA, 'auto');
 
@@ -27,15 +21,11 @@ if (typeof window !== 'undefined') {
   // https://github.com/googleanalytics/autotrack
 
   // most important plugin for phenomic
-  require('autotrack/lib/plugins/url-change-tracker');
   ga('require', 'urlChangeTracker');
 
   // some plugins you might like
-  require('autotrack/lib/plugins/clean-url-tracker');
   ga('require', 'cleanUrlTracker');
-  require('autotrack/lib/plugins/outbound-form-tracker');
   ga('require', 'outboundFormTracker');
-  require('autotrack/lib/plugins/outbound-link-tracker');
   ga('require', 'outboundLinkTracker');
 
   // check out more here https://github.com/googleanalytics/autotrack#plugins
@@ -44,8 +34,9 @@ if (typeof window !== 'undefined') {
   ga('send', 'pageview');
   console.log('pageview');
 }
+/* eslint-enable */
 
-const AppContainer = ({ location: { query }, children }) => {
+const AppContainer = ({location: {query}, children}) => {
   console.log(JSON.stringify(query));
   const intlConfig = getIntlForLocale(query.locale);
 
@@ -55,7 +46,7 @@ const AppContainer = ({ location: { query }, children }) => {
         <DefaultHeadMeta
           scripts={[
             // GOOGLE Analytics, part 2/2
-            { async: true, src: 'https://www.google-analytics.com/analytics.js' },
+            {async: true, src: 'https://www.google-analytics.com/analytics.js'},
           ]}
         />
         <Content>
