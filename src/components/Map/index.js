fec2e323e72f9e40f5bf9c3ce1479ccf584cdd78
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-05 16:40:29 +0600

    feat(Map): make map responsive

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,53 +1,26 @@
 /* @flow */
 
-import React, {Component} from 'react';
-import Measure from 'react-measure';
+import React from 'react';
+import cx from 'classnames';
 
 import styles from './index.css';
 
 const API_KEY = 'AIzaSyDHzwnCkK6DwTntBvi2avh6imL2Y5W_Ts0';
-const googleMapURL = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=Space+Needle,Seattle+WA`;
+const googleMapURL = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=Intellection`;
 
-type MapStateType = {|
-  width: number,
-  height: number,
-|};
-
-const Iframe = (width, height) => (
-  <iframe
-    title={'Google map'}
-    width={width}
-    height={height}
-    frameBorder="0"
-    src={googleMapURL}
-    allowFullScreen
-  />
+const Map = ({className}) => (
+  <div
+    className={cx(styles.intrinsicContainer, {
+      [className]: !!className,
+    })}
+  >
+    <iframe
+      title={'Google map'}
+      frameBorder="0"
+      src={googleMapURL}
+      allowFullScreen
+    />
+  </div>
 );
-
-class Map extends Component {
-  state: MapStateType = {
-    width: 675,
-    height: 100,
-  };
-
-  handleBackgroundMeasure = dimensions =>
-    console.log(dimensions) ||
-    this.setState({
-      width: this.container.width,
-      height: this.container.height,
-    });
-
-  render() {
-    return (
-      <div ref={el => (this.container = el)} className={styles.container}>
-        <Measure onMeasure={this.handleBackgroundMeasure}>
-          <div className={styles.intrinsicContainer}>
-            <Iframe {...this.state} />
-          </div>
-        </Measure>
-      </div>
-    );
-  }
-}
 
 export default Map;
