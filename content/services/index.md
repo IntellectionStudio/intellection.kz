ed07fd635999d0918f3a716c40f5dbd84accdf02
White <apple@MacBook-Pro-apple-2.local>
2017-08-07 14:28:41 +0600

    feat: services page

---
+++ content/services/index.md
@@ -0,0 +1,4 @@
+---
+title: Наши услуги
+layout: ServicesPage
+---
---
+++ src/layouts/ServicesPage/index.js
@@ -0,0 +1,178 @@
+import React, {Component} from 'react';
+
+import Page from 'layouts/Page';
+
+import styles from './index.css';
+
+class ServicesPage extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  handleChandle(property, value) {
+    this.setState({[property]: value});
+    console.log(this.state.name, this.state.email, this.state.message);
+  }
+  submit = () => {
+    fetch('https://mandrillapp.com/api/1.0/messages/send.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        key: 'B1AoYTIo1KTLERLF1WOiRg',
+        message: {
+          from_email: 'info@intellection.kz',
+          to: [
+            {
+              email: 'intellection.kz@gmail.com',
+              type: 'to',
+            },
+          ],
+          subject: 'APPLICATION FORM (intellection.kz)',
+          html: `Name: ${this.state.name} <br />Email: ${this.state.email} <br />Message: ${this.state.message} <br />`,
+        },
+      }),
+    })
+      .then((res) =>  // eslint-disable-line
+        this.success()
+      ) // eslint-disable-line promise/prefer-await-to-then
+      .catch(err => {
+        // TODO
+        console.log(err);
+      });
+  };
+  success = () => {
+    // document.getElementById('comment').val('');
+    // document.getElementById('name').val('');
+    // // document.getElementById('email').val('');
+    document.getElementById('submit').style.visibility = 'hidden';
+    alert('Hello White');
+  };
+  render() {
+    return (
+      <Page {...Page.pickPageProps(this.props)}>
+        <div className={styles.container}>
+          <div className={styles.intro}>
+            <div className={styles.intro1}>
+              <h1>
+                Have an Uber like App idea?
+              </h1>
+              <h1> We can power it </h1>
+              <p>
+                As experts in this space,
+                we understand the core technology
+                that will make your On Demand Idea click.
+                We are dedicated to deliver fully customized
+                solutions and develop Uber clones for your specific business model.
+              </p>
+              <h2>
+                Why choose us?
+              </h2>
+              <ul className={styles.lists}>
+                <li className={styles.list}>
+                  Вы научитесь писать код
+                </li>
+                <li className={styles.list}>
+                  Вы научитесь писать код
+                </li>
+                <li className={styles.list}>
+                  Вы научитесь писать код
+                </li>
+                <li className={styles.list}>
+                  Вы научитесь писать код
+                </li>
+              </ul>
+            </div>
+            <div className={styles.intro2}>
+              <div className={styles.form}>
+                <h1>Create your Marketplace App</h1>
+                <p>Let Juggernaut power your On-Demand App</p>
+                <div className={styles.firstForm}>
+                  <div className={styles.nameForm}>
+                    <p style={{color: '#a8acb9'}}>YOUR NAME</p>
+                    <input
+                      id={styles.name}
+                      className={styles.textInput}
+                      placeholder="Damir"
+                      onChange={a => this.handleChandle('name', a.target.value)}
+                    />
+                  </div>
+                  <div className={styles.emailForm}>
+                    <p style={{color: '#a8acb9'}}>EMAIL</p>
+                    <input
+                      id={styles.email}
+                      className={styles.textInput3}
+                      placeholder="damir331company@gmail.com"
+                      onChange={b =>
+                        this.handleChandle('email', b.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className={styles.secondForm} />
+                <p style={{color: '#a8acb9', textAlign: 'left'}}>
+                  ABOUT PROJECT
+                </p>
+                <textarea
+                  className={styles.textInput2}
+                  rows="7"
+                  cols="50"
+                  name="comment"
+                  form="usrform"
+                  id="comment"
+                  onChange={c => this.handleChandle('message', c.target.value)}
+                />
+                <button
+                  id={styles.submit}
+                  type="reset"
+                  value="Reset"
+                  onClick={this.submit}
+                >
+                  GET STARTED WITH YOUR IDEA
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.process}>
+            <div className={styles.process1}>
+              <h1 style={{textAlign: 'center', paddingTop: '33px'}}>
+                Process
+              </h1>
+            </div>
+            <div className={styles.process2}>
+              <div className={styles.icon1}>
+                <div className={styles.circle1}>
+                  <img alt="" src={'/assets/icons/circle1.png'} />
+                </div>
+                <p>Analysis and planning</p>
+              </div>
+              <div className={styles.icon2}>
+                <div className={styles.circle2} />
+                <img alt="" src={'/assets/icons/circle2.png'} />
+                <p>Design</p>
+              </div>
+              <div className={styles.icon3}>
+                <div className={styles.circle3} />
+                <img alt="" src={'/assets/icons/circle3.png'} />
+                <p>Development</p>
+              </div>
+              <div className={styles.icon4}>
+                <div className={styles.circle4} />
+                <img alt="" src={'/assets/icons/circle4.png'} />
+                <p>Support</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.projects}>
+            <div className={styles.ptitle}>
+              <h2>
+                Some of projects we have built
+              </h2>
+            </div>
+          </div>
+        </div>
+      </Page>
+    );
+  }
+}
+
+export default ServicesPage;
