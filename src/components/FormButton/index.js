c48e3deadb604d2cae306d69a7379bdd38e29ec3
adilkhan <adilkhankenzhetaev@gmail.com>
2017-08-07 20:43:26 +0600

    feat(FormButton): rename component to FormButton

---
+++ src/components/FormButton/index.js
@@ -0,0 +1,19 @@
+import {pure} from 'recompact';
+import React from 'react';
+import cx from 'classnames';
+
+import styles from './index.css';
+
+const FormButton = ({
+  params: {type, value, containerClassName, inputClassName},
+}) => (
+  <div
+    className={cx(styles.root, {
+      [containerClassName]: !!containerClassName,
+    })}
+  >
+    <input className={inputClassName} type={type} value={value} />
+  </div>
+);
+
+export default pure(FormButton);
